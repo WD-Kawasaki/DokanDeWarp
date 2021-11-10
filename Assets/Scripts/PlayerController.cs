a84ec82d802b8ee8b794f8a1b349df4e453b08c0
TakaoWing <tt.wing2838783@gmail.com>
2021-11-10 16:04:43 +0900

    プレイヤーの移動とワープ機能の完成
    操作方法や動きに難あり

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 10; // Player move speed
+    [SerializeField]
+    private float rotateSpeed = 5; // Player rotate speed
+    [SerializeField]
+    private float jumpPower = 10; // Player hump power
+    private Rigidbody rb;
+    private bool pushJampButton = false;
+    private bool isGround = false;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxis("Horizontal");
+        this.transform.Rotate(new Vector3(0, rotateSpeed * h * Time.deltaTime, 0));
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+
+        pushJampButton = Input.GetKeyDown(KeyCode.Space);
         
     }
+
+    private void FixedUpdate() {
+        if (pushJampButton&&isGround) {
+            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+            isGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Ground") {
+            isGround = true;
+        }
+    }
+
+
 }
--- Assets/Scripts/WarpController.cs
+++ Assets/Scripts/WarpController.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WarpController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject toDokan = null;
+    private bool isPushWarpButton = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,15 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isPushWarpButton = Input.GetKeyDown(KeyCode.DownArrow);
         
+       
     }
+
+    private void OnCollisionStay(Collision collision) {
+        if(collision.gameObject.tag == "Player" && this.transform.position.y < collision.gameObject.transform.position.y && isPushWarpButton) {
+            collision.gameObject.transform.position = toDokan.transform.position + new Vector3(0, 0.01f, 0);
+        }
+    }
+
 }
